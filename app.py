ab6ccd47c5d5d814654ef1e9b3e395cc1fefd765
Gimb0 <bradgimbo@gmail.com>
2019-07-29 17:46:45 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+with open("52_Ta_18_Ar1100_1.dp_rpc_asc") as data_file:
+    for line in data_file:
+        # Check for Sample ID
+        if(line):
+            # dataObject.sampleID = line[]
+            print(line)
+        # Check for Date
+        elif(line):
+            # dataObject.sampleDate = line[]
+            print(line)
+        # Do this for every line 
