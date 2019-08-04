8553f7e5b63cbd2f2c8dc1b3edf8718da197fe28
Gimb0 <bradgimbo@gmail.com>
2019-08-04 20:29:29 +1000

    Open and display data file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 
 # PyQt5 text editor example
 # https://gist.github.com/olivierkes/a5ed456fb4313d248042b3ff29f5e754
@@ -6,43 +6,78 @@
 import sys
 import h5py
 import numpy
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QFileDialog, QMessageBox
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import * #QApplication, QMainWindow, QAction, qApp, QFileDialog, QLabel
+from PyQt5.QtGui import * #QIcon
+from regex import match
 
-class SIMS_Window(QMainWindow):
+class DataSample():
+    sampleID = None
+    sampleDate = None
+    sampleTime = None
+
+    def setSampleID(id):
+        sampleID = id
+    
+    def getSampleID():
+        return sampleID
+
+    def setSampleDate(date):
+        sampleDate = date
+
+    def setSampleTime(time):
+        sampleTime = time
+
+class Main(QMainWindow):
+    # ----- Variables -----
+    ds = DataSample()
+
+
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.initUI()        
 
     def initUI(self):
+        # ----- Windows Settings -----
+        self.resize(400, 420)
+        self.setWindowTitle('SIMS Data Extractor')
+
+
+        # ----- Menubar -----
         menubar = self.menuBar()
         # menubar.setNativeMenuBar(False) # Add menubar to top of window for Mac OS
         fileMenu = menubar.addMenu('File')
         
-        self.resize(400, 420)
-        self.setWindowTitle('SIMS Data Extractor')
-
         dlg = QFileDialog(self)
         openAction = QAction('Open File', self)
-        openAction.triggered.connect(self.selectDataFile)
+        openAction.triggered.connect(self.openDataFile)
         fileMenu.addAction(openAction)
-    
-    def openDataFile(self, filename):
-        with open(filename) as data_file:
-            for line in data_file:
-                print(line)            
 
-    def selectDataFile(self):
+
+        # ----- Display Text -----
+        self.text = QTextEdit(self)
+        self.setCentralWidget(self.text)
+    
+    def openDataFile(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File')
-        #openDataFile(self, filename)
-        with open(filename) as dataFile:
-            for lines in dataFile:
-                msg = QLabel(lines, self)
-                break
+        info = ""
+        with open(filename[0], 'r') as f:
+            for line in f:
+                if match(r'^Sample ID', line): # Extract Sample ID
+                    ID_line = line.split(' ')[1]
+                    ID_line = ID_line.strip('ID')
+                    ID_line = ID_line.strip()
+                    self.ds.setSampleID(ID_line)
+                    info += ID_line + "\n"
+                elif match(r'^Analysis date', line): # Extract sample date
+                    info += line
+                elif match(r'^Analysis time', line): # Extract sample time
+                    info += line
+        self.text.setText(info)
+        print(self.ds.getSampleID())
 
 def main():
     app = QApplication(sys.argv)
-    sims = SIMS_Window()
+    sims = Main()
     sims.show()
     app.exec_()
 
