e18d3dc3b3d72b00de6fee536f310961c3c78797
Gimb0 <bradgimbo@gmail.com>
2019-08-08 11:49:04 +1000

    Update

--- app.py
+++ app.py
@@ -15,17 +15,24 @@ class DataSample():
     sampleDate = None
     sampleTime = None
 
-    def setSampleID(id):
-        sampleID = id
+    def setSampleID(self, id):
+        self.sampleID = id
     
-    def getSampleID():
-        return sampleID
+    def getSampleID(self):
+        return self.sampleID
 
-    def setSampleDate(date):
-        sampleDate = date
+    def setSampleDate(self, date):
+        self.sampleDate = date
 
-    def setSampleTime(time):
-        sampleTime = time
+    def setSampleTime(self, time):
+        self.sampleTime = time
+
+    def printClassVars(self):
+        print('# ---- Class Info ----')
+        print('Sample ID is: {}'.format(self.sampleID))
+        print('Sample Date is: {}'.format(self.sampleDate))
+        print('Sample Time is: {}'.format(self.sampleTime))
+
 
 class Main(QMainWindow):
     # ----- Variables -----
@@ -63,17 +70,26 @@ class Main(QMainWindow):
         with open(filename[0], 'r') as f:
             for line in f:
                 if match(r'^Sample ID', line): # Extract Sample ID
-                    ID_line = line.split(' ')[1]
-                    ID_line = ID_line.strip('ID')
-                    ID_line = ID_line.strip()
-                    self.ds.setSampleID(ID_line)
-                    info += ID_line + "\n"
+                    IDLine = line.split(' ')[1]
+                    IDLine = IDLine.strip('ID')
+                    IDLine = IDLine.strip()
+                    self.ds.setSampleID(IDLine)
+                    info += IDLine + "\n"
                 elif match(r'^Analysis date', line): # Extract sample date
-                    info += line
+                    dateLine = line.split(' ')[1]
+                    dateLine = dateLine.strip('date')
+                    dateLine = dateLine.strip()
+                    self.ds.setSampleDate(dateLine)
+                    info += dateLine + "\n"
                 elif match(r'^Analysis time', line): # Extract sample time
-                    info += line
+                    timeLine = line.split(' ')[1]
+                    timeLine = timeLine.strip('time')
+                    timeLine = timeLine.strip()
+                    self.ds.setSampleTime(timeLine)
+                    info += timeLine + '\n'
+                
         self.text.setText(info)
-        print(self.ds.getSampleID())
+        self.ds.printClassVars()
 
 def main():
     app = QApplication(sys.argv)
