629d0a468058e22b5b1664b22608c1d4e7159d5a
Gimb0 <bradgimbo@gmail.com>
2019-08-11 19:48:38 +1000

    Cleaned regex and Extract species

--- app.py
+++ app.py
@@ -8,12 +8,29 @@ import h5py
 import numpy
 from PyQt5.QtWidgets import * #QApplication, QMainWindow, QAction, qApp, QFileDialog, QLabel
 from PyQt5.QtGui import * #QIcon
-from regex import match
+from regex import *
+
+class Specie():
+
+    def __init__(self, t, massA, massF, d, wt):
+        self.speciesType = t
+        self.speciesMassAMU = massA
+        self.speciesMassField = massF
+        self.detector = d
+        self.WT = wt
+
+    def getSpecieInfo(self):
+        str = "Type is: " + self.speciesType + "\n"
+        str += "Species Mass (amu) is: " + self.speciesMassAMU + "\n"
+        str += "Species Mass (field) is: " + self.speciesMassField + "\n"
+        str += "Detector is: " + self.detector + "\n"
+        str += "WT (s) is: " + self.WT + "\n\n"
+        return str
 
 class DataSample():
-    sampleID = None
-    sampleDate = None
-    sampleTime = None
+
+    def __init__(self):
+        self.species = []
 
     def setSampleID(self, id):
         self.sampleID = id
@@ -27,11 +44,25 @@ class DataSample():
     def setSampleTime(self, time):
         self.sampleTime = time
 
-    def printClassVars(self):
-        print('# ---- Class Info ----')
-        print('Sample ID is: {}'.format(self.sampleID))
-        print('Sample Date is: {}'.format(self.sampleDate))
-        print('Sample Time is: {}'.format(self.sampleTime))
+    def setSampleAcqTime(self, acqTime):
+        self.totAcquisitionTime = acqTime
+
+    def addSpecies(self, sType, sMassAMU, sMassField, detector, WT):
+        self.species.append(Specie(sType, sMassAMU, sMassField, detector, WT))
+
+    def getClassVars(self):
+        str = '# ---- Class Info ----\n'
+        str += 'Sample ID is: ' + self.sampleID + '\n'
+        str += 'Sample Date is: ' + self.sampleDate + '\n'
+        str += 'Sample Time is: ' + self.sampleTime + '\n'
+        str += 'Total Acquisition time (s): ' + self.totAcquisitionTime + '\n'
+        return str
+
+    def getSpecies(self):
+        str = "# ---- Species Info ----\n"
+        for s in self.species:
+            str += s.getSpecieInfo()
+        return str
 
 
 class Main(QMainWindow):
@@ -67,29 +98,49 @@ class Main(QMainWindow):
     def openDataFile(self):
         filename = QFileDialog.getOpenFileName(self, 'Open File')
         info = ""
+        ds = DataSample
+        # Extract metadata not actual data
+        with open(filename[0], 'r') as f:
+            dfString = f.read()
+            # Find all sample ID values in data and store first one
+            sampleIdRegex = search(r'Sample ID\t\t(\w*|\W*)', dfString)
+            self.ds.setSampleID(sampleIdRegex.group(1))
+
+            analysisDateRegex = search(r'Analysis date\t\t(\d{2}\/\d{2}\/\d{4})', dfString)
+            self.ds.setSampleDate(analysisDateRegex.group(1))
+
+            analysisTimeRegex = search(r'Analysis time\t\t(\d{2}:\d{2})', dfString)
+            self.ds.setSampleTime(analysisTimeRegex.group(1))
+
+            acquisitionRegex = search(r'Total acquisition time \(s\)\t(\d+)', dfString)
+            self.ds.setSampleAcqTime(acquisitionRegex.group(1))
+
+        # Loop through file for data
+        counter = -1
+        data = ""
         with open(filename[0], 'r') as f:
             for line in f:
-                if match(r'^Sample ID', line): # Extract Sample ID
-                    IDLine = line.split(' ')[1]
-                    IDLine = IDLine.strip('ID')
-                    IDLine = IDLine.strip()
-                    self.ds.setSampleID(IDLine)
-                    info += IDLine + "\n"
-                elif match(r'^Analysis date', line): # Extract sample date
-                    dateLine = line.split(' ')[1]
-                    dateLine = dateLine.strip('date')
-                    dateLine = dateLine.strip()
-                    self.ds.setSampleDate(dateLine)
-                    info += dateLine + "\n"
-                elif match(r'^Analysis time', line): # Extract sample time
-                    timeLine = line.split(' ')[1]
-                    timeLine = timeLine.strip('time')
-                    timeLine = timeLine.strip()
-                    self.ds.setSampleTime(timeLine)
-                    info += timeLine + '\n'
-                
-        self.text.setText(info)
-        self.ds.printClassVars()
+                if data == "" and counter == -1: # Look for data sections
+                    if match(r'\*{3} MEASUREMENT CONDITIONS, \w+\.dp \*{3}:', line):
+                        data = "MC"
+                        counter = 3
+                elif counter > 0:
+                    counter -= 1
+                    continue
+                else:
+                    if counter == 0:
+                        if data == "MC":
+                            while line != "\n":
+                                s = line.replace(" ", "")
+                                s = s.split('|')
+                                s[-1] = s[-1].strip()
+                                self.ds.addSpecies(s[0], s[1], s[2], s[3], s[4])
+                                line = f.readline()
+                            break
+
+        print(self.ds.getClassVars())
+        print(self.ds.getSpecies())
+        self.text.setText(self.ds.getClassVars() + self.ds.getSpecies())
 
 def main():
     app = QApplication(sys.argv)
