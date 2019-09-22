6d507acb7b7c2a7d0e4148324c66495bfda4bb2d
Gimb0 <bradgimbo@gmail.com>
2019-09-22 21:59:55 +1000

    Extract metadata from data file

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 
 import sys
 from PyQt5 import QtWidgets, uic
+import io
+import pandas as pd;
+from datetime import timedelta, time
+
 
 class MainUI(QtWidgets.QMainWindow):
 
@@ -15,11 +19,96 @@ class MainUI(QtWidgets.QMainWindow):
 
         self.show()
 
+    def extractSimsData(self, handle):
+        databuf = io.StringIO()
+        self.dataPoints = 0
+        for _ in range(4):
+            next(handle)
+        for line in handle:
+            if line.startswith('*** DATA END ***'):
+                databuf.seek(io.SEEK_SET)
+                return databuf
+            databuf.write(line)
+            self.dataPoints += 1
+    
+    def extractSpeciesList(self, handle):
+        names = []
+
+        for _ in range(3):
+            next(handle)
+        for line in handle:
+            if line.rstrip():
+                name = line.split()[0]
+                names.append(name)
+                # names.extend([f'time-{name}', f'count-{name}'])
+            else:
+                return names
+
+    def updateInterface(self):
+        # try:
+        self.sampleIDText = self.findChild(QtWidgets.QLineEdit, 'sampleIDText')
+        self.sampleIDText.setText(self.sampleID)
+
+        self.sampleDateDate = self.findChild(QtWidgets.QLineEdit, 'sampleDateDate')
+        self.sampleDateDate.setText(self.analysisDate)
+        # self.sampleDateDate = self.findChild(QtWidgets.QDateEdit, 'sampleDateDate')
+        # self.sampleDateDate.setDate(self.analysisDate)
+        
+        self.acqTimeTime = self.findChild(QtWidgets.QLineEdit, 'sampleAcqTimeTime')
+        self.acqTimeTime.setText(self.acqTime)
+
+        self.speciesListText = self.findChild(QtWidgets.QLineEdit, 'speciesListText')
+        self.speciesListText.setText(self.speciesListString)
+        
+        self.pIonsText = self.findChild(QtWidgets.QLineEdit, 'samplePriIONText')
+        self.pIonsText.setText(self.pIons)
+
+        self.pIonsEnergyText = self.findChild(QtWidgets.QLineEdit, 'samplePriIONEText')
+        self.pIonsEnergyText.setText(self.pIonsEnergy)
+
+        self.dataPointsText = self.findChild(QtWidgets.QLineEdit, 'sampleDataPointsText')
+        self.dataPointsText.setText(str(self.dataPoints))
+        # except:
+        #     pass
 
     def openDataFile(self):
         try:
             filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
-            # with open(filename[0], 'r') as dataFile:
+            with open(filename[0], 'rt') as dataFile:
+                # Check if datafile is valid
+                if not dataFile.readline().startswith('*** DATA FILES ***'):
+                    return
+                # Extract data from data file
+                for line in dataFile:
+                    if line.startswith('Sample ID'):
+                        self.sampleID = line.split()[-1]
+                    if line.startswith('Analysis date'):
+                        self.analysisDate = line.split()[-1]
+                    if line.startswith('*** DATA START ***'):
+                        self.simsdata = self.extractSimsData(dataFile)
+                    if line.startswith('Total acquisition time (s)'):
+                        self.acqTime = int(line.split()[-1])
+                        self.acqTime = str(timedelta(seconds=self.acqTime))
+                    if line.startswith("*** MEASUREMENT CONDITIONS"):
+                        speciesList = self.extractSpeciesList(dataFile)
+                        self.header = []
+                        self.speciesListString = ""
+                        for specie in speciesList:
+                            self.speciesListString += specie + ", "
+                            self.header.extend([f'time-{specie}', f'count-{specie}'])
+                        self.speciesListString = self.speciesListString[0:-2]
+                    if line.startswith("Primary ions"):
+                        self.pIons = line.split()[-1]
+                    if line.startswith("Impact energy"):
+                        self.pIonsEnergy = line.split()[-1]
+                        
+            self.updateInterface()
+            
+            # Create pandas data frame
+            self.df = pd.read_csv(self.simsdata, header=None, delim_whitespace=True, skip_blank_lines=True)
+            self.simsdata.close()
+            self.df.columns = self.header
+
         except FileNotFoundError:
             # A file was not chosen so do nothing
             pass
